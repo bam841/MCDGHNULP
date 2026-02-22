3c1e47704e526da33fda8d8dc4629db6c9b7dad0
John Abrahm <zapicoja@students.nu-lipa.edu.ph>
2026-02-22 14:43:06 +0800

    Initial POS layout

---
+++ webposfolder/pos-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-50">
+      <h1 className="text-4xl font-bold text-blue-600 underline">
+        Tailwind is Working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
